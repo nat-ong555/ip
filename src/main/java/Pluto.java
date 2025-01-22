f3903bc4a60f5903a85bad7a377a4f7565b81a8c
nat-ong555 <natalieong555@gmail.com>
2025-01-23 02:23:38 +0800

    Echo user commands

--- src/main/java/Pluto.java
+++ src/main/java/Pluto.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Pluto {
     public static void main(String[] args) {
         String greeting = "Hi! I am Pluto\n" + "What can I do for you today? \n\n";
         String goodbye = "Bye! Hope to see you again :)";
-        System.out.println(greeting + goodbye);
+        System.out.println(greeting);
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(goodbye);
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }
