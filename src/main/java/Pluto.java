d903efe7ba1881ab195fb88501b3876a229fa529
nat-ong555 <natalieong555@gmail.com>
2025-01-23 02:11:33 +0800

    Renamed chatbot + added greeting

---
+++ src/main/java/Pluto.java
@@ -0,0 +1,7 @@
+public class Pluto {
+    public static void main(String[] args) {
+        String greeting = "Hi! I am Pluto\n" + "What can I do for you today? \n\n";
+        String goodbye = "Bye! Hope to see you again :)";
+        System.out.println(greeting + goodbye);
+    }
+}
