60ffe6d721a141383fa219a87fcd006540384915
nat-ong555 <natalieong555@gmail.com>
2025-01-26 01:48:20 +0800

    handle errors using exception class

--- src/main/java/Pluto.java
+++ src/main/java/Pluto.java
@@ -16,62 +16,75 @@ public class Pluto {
         String input;
 
         while (true) {
-            input = scanner.nextLine().trim();
+            try {
+                input = scanner.nextLine().trim();
 
-            if (input.equals("bye")) {
-                message.showGoodbyeMessage();
-                break;
-            } else if (input.equals("list")) {
-                taskList.listTasks();
-                continue;
-            }
-
-            String[] parts = input.split(" ", 2);
-            String command = parts[0];
-
-            switch (command) {
-                case "mark":
-                    int markIndex = Integer.parseInt(parts[1]) - 1;
-                    taskList.markTask(markIndex);
+                if (input.equals("bye")) {
+                    message.showGoodbyeMessage();
                     break;
+                } else if (input.equals("list")) {
+                    taskList.listTasks();
+                    continue;
+                }
 
-                case "unmark":
-                    int unmarkIndex = Integer.parseInt(parts[1]) - 1;
-                    taskList.unmarkTask(unmarkIndex);
-                    break;
+                String[] parts = input.split(" ", 2);
+                String command = parts[0];
 
-                case "todo":
-                    if (parts.length < 2) {
-                        System.out.println("OOPS! The description of a todo cannot be empty.");
-                    } else {
-                        ToDo todo = new ToDo(parts[1]);
-                        taskList.addTask(todo);
-                    }
-                    break;
+                switch (command) {
+                    case "mark":
+                        if (parts.length < 2) {
+                            throw new PlutoException("Please provide a task number to mark.");
+                        }
+                        int markIndex = Integer.parseInt(parts[1]) - 1;
+                        taskList.markTask(markIndex);
+                        break;
 
-                case "deadline":
-                    if (parts.length < 2 || !parts[1].contains(" /by ")) {
-                        System.out.println("OOPS! The deadline format is incorrect.");
-                    } else {
-                        String[] deadlineParts = parts[1].split(" /by ", 2);
-                        Deadline deadline = new Deadline(deadlineParts[0], deadlineParts[1]);
-                        taskList.addTask(deadline);
-                    }
-                    break;
+                    case "unmark":
+                        if (parts.length < 2) {
+                            throw new PlutoException("Please provide a task number to unmark.");
+                        }
+                        int unmarkIndex = Integer.parseInt(parts[1]) - 1;
+                        taskList.unmarkTask(unmarkIndex);
+                        break;
 
-                case "event":
-                    if (parts.length < 2 || !parts[1].contains(" /from ") || !parts[1].contains(" /to ")) {
-                        System.out.println("OOPS! The event format is incorrect.");
-                    } else {
-                        String[] eventParts = parts[1].split(" /from | /to ", 3);
-                        Event event = new Event(eventParts[0], eventParts[1], eventParts[2]);
-                        taskList.addTask(event);
-                    }
-                    break;
+                    case "todo":
+                        if (parts.length < 2) {
+                            throw new PlutoException("The description of a todo cannot be empty.");
+                        } else {
+                            ToDo todo = new ToDo(parts[1]);
+                            taskList.addTask(todo);
+                        }
+                        break;
 
-                default:
-                    System.out.println("OOPS! I'm sorry, but I don't know what that means :-(");
-                    break;
+                    case "deadline":
+                        if (parts.length < 2 || !parts[1].contains(" /by ")) {
+                            throw new PlutoException("The deadline format is incorrect. Please use: deadline <task> /by <time>");
+                        } else {
+                            String[] deadlineParts = parts[1].split(" /by ", 2);
+                            Deadline deadline = new Deadline(deadlineParts[0], deadlineParts[1]);
+                            taskList.addTask(deadline);
+                        }
+                        break;
+
+                    case "event":
+                        if (parts.length < 2 || !parts[1].contains(" /from ") || !parts[1].contains(" /to ")) {
+                            throw new PlutoException("The event format is incorrect. Please use: event <task> /from <start> /to <end>");
+                        } else {
+                            String[] eventParts = parts[1].split(" /from | /to ", 3);
+                            Event event = new Event(eventParts[0], eventParts[1], eventParts[2]);
+                            taskList.addTask(event);
+                        }
+                        break;
+
+                    default:
+                        throw new PlutoException("I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (PlutoException e) {
+                System.out.println("OOPS! " + e.getMessage());
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("OOPS! Task number is out of range");
+            } catch (NumberFormatException e) {
+                System.out.println("OOPS! Task number must be a valid number.");
             }
         }
 
